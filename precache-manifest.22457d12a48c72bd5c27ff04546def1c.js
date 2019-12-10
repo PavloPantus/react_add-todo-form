5c1ecad98da25a9f95f3e8da6237497395c896d3
PavloPantus <mr.pantus@gmail.com>
2019-12-11 00:17:11 +0200

    Updates

---
+++ precache-manifest.22457d12a48c72bd5c27ff04546def1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b746d1217349442192bd43caf98dd812",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "82bf9250ef9d519c5adc",
+    "url": "/react_add-todo-form/static/css/main.bc24fc50.chunk.css"
+  },
+  {
+    "revision": "59c8687e1216867c77be",
+    "url": "/react_add-todo-form/static/js/2.07ca2f4b.chunk.js"
+  },
+  {
+    "revision": "82bf9250ef9d519c5adc",
+    "url": "/react_add-todo-form/static/js/main.67ae0394.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
